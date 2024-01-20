a7fb10f2fa12ec2d5a0e9640385bdb58410ccb9d
Claudia Bergstaller <claudia.bergstaller@gmx.com>
2024-01-20 00:51:54 +0000

    Add sort functionality for price

--- products/views.py
+++ products/views.py
@@ -9,8 +9,22 @@ def all_products (request):
     query = None
     categories = None
     formats = None
+    sort = None
+    direction = None
 
     if request.GET:
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
@@ -31,11 +45,14 @@ def all_products (request):
             queries = Q(name__icontains=query) | Q(technique__icontains=query) | Q(size__icontains=query) | Q(category__name__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
         'current_format': formats,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
