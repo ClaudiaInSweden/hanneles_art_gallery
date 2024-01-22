2f037e38ffebf9e757790b705009d757b169eed6
Claudia Bergstaller <claudia.bergstaller@gmx.com>
2024-01-22 00:35:22 +0000

    Update shopping cart layout

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -11,7 +11,7 @@ def view_cart(request):
 
 def add_to_cart(request, item_id):
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -20,7 +20,7 @@ def add_to_cart(request, item_id):
         cart[item_id] +- quantity
     else:
         cart[item_id] = quantity
-        messages.error(request, f'Painting "{ product.name }" added to your shopping cart.')
+        messages.success(request, f'Painting "{ product.name }" has been added to your shopping cart.')
 
     request.session['cart'] = cart
     return render(request, 'cart/cart.html')
@@ -43,11 +43,14 @@ def update_cart(request, item_id):
 def remove_from_cart(request, item_id):
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         cart = request.session.get('cart', {})
         cart.pop(item_id)
+        messages.success(request, f'Painting "{ product.name }" has been removed from your shopping cart.')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Painting could not be removed: {e}')
         return HttpResponse(status=500)
