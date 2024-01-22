b4164215c85fc96b1c853d3e07c62cd8fee40c06
Claudia Bergstaller <claudia.bergstaller@gmx.com>
2024-01-22 22:06:15 +0000

    Add basic checkout functionality

--- art_gallery/settings.py
+++ art_gallery/settings.py
@@ -165,8 +165,13 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+# Delivery and Stripe
 STANDARD_DELIVERY_COST = 40
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
