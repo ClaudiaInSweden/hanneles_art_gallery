e60c5424ecd1ca243bd256ab0f0feaf036285b06
Claudia Bergstaller <claudia.bergstaller@gmx.com>
2024-01-23 23:03:04 +0000

    Complete webhook handler

--- checkout/models.py
+++ checkout/models.py
@@ -16,13 +16,15 @@ class Order(models.Model):
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=False, blank=False)
     town = models.CharField(max_length=100, null=False, blank=False)
     country = models.CharField(max_length=50, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     delivery = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_cart = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
