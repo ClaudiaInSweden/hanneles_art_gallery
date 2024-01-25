d23a60cef229615fd1bf8a3139b933453206d765
Claudia Bergstaller <claudia.bergstaller@gmx.com>
2024-01-25 15:53:24 +0000

    Add function to delete paintings for admin

--- products/views.py
+++ products/views.py
@@ -76,9 +76,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'The painting was added successfully!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add painting! Please ensure that the form is valid!')
     else:
@@ -113,3 +113,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a painting """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Painting "{product.name}" has been deleted!')
+    return redirect(reverse('products'))
