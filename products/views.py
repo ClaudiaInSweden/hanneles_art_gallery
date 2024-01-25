d65bd1f84cd972d91fa88d47583796ee57553558
Claudia Bergstaller <claudia.bergstaller@gmx.com>
2024-01-25 13:12:25 +0000

    Add product function and template finished

--- products/views.py
+++ products/views.py
@@ -72,7 +72,17 @@ def product_detail(request, product_id):
 
 
 def add_product(request):
-    form = ProductForm()
+    """ Add a new painting to the store """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'The painting was added successfully!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add painting! Please ensure that the form is valid!')
+    else:
+        form = ProductForm()
     template = 'products/add_product.html'
     context = {
         'form': form,
