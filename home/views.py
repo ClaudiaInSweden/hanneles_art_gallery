9e67c1a7a64f2ecbf5040da1a4ffa3753965ed9b
Claudia Bergstaller <claudia.bergstaller@gmx.com>
2024-01-27 20:14:06 +0000

    Add hover effect and text to delete and edit icons on product and cart pages

--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index (request):
 
     return render(request, 'home/index.html')
+
+
+def privacy_policy (request):
+
+    return render(request, 'home/privacy_policy.html')
