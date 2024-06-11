912386e2021410aaa9640d8a3e31a73f4b07b096
Claudia Bergstaller <claudia.bergstaller@gmx.com>
2024-06-11 18:40:40 +0000

    Update bootstrap cards layout on blog template

--- academy/urls.py
+++ academy/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='blog'),
-    path('blog/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
     # path('add_post/', views.add_post, name='add_post'),
 ]
