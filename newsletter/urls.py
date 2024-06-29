cb627e314256351fd36e2251d2b95f37f23bfc71
Claudia Bergstaller <claudia.bergstaller@gmx.com>
2024-06-29 11:11:43 +0000

    Add styling to subscribe template

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('newsletter/subscribe/', views.subscribe, name='subscribe'),
-    path('newsletter/mailcontent/', views.mail_content, name='mail_content')
+    path('subscribe/', views.subscribe, name='subscribe'),
+    path('newsletter/', views.mail_content, name='mail_content')
 ]
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,20 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Subscribers, MailContent
+from .forms import SubscribeForm, MailContentForm
 from django.contrib import messages
 from django.core.mail import send_mail
 
 # Create your views here.
 def subscribe(request):
+    if request.method == 'POST':
+        form = SubscribeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'You have successfully subscribed to our newsletter!')
+            return redirect('index.html')
+    else:
+        form = SubscribeForm()
 
     context = {
-
+        'form': form,
     }
     return render(request, 'newsletter/subscribe.html', context)
 
 
 def mail_content(request):
-
+    if request.method == 'POST':
+        form = MailContentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'The newsletter has been sent!')
+            return redirect('index.html')
+    else:
+        form = MailContentForm()
     context = {
-
+        'form': form
     }
     return render(request, 'newsletter/mailcontent.html', context)
